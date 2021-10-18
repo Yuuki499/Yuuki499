f6f458eace84f021e38b2cb2b60a30be703e31ad
Yuuki499 <62681720+Yuuki499@users.noreply.github.com>
2021-10-17 22:12:46 -0300

    Create ping.js

---
+++ SlashCommands/info/ping.js
@@ -0,0 +1,30 @@
+const { Client, CommandInteraction } = require("discord.js");
+
+module.exports = {
+
+    name: "ping",
+
+    description: "Pong!",
+
+    type: 'CHAT_INPUT',
+
+    /**
+
+     *
+
+     * @param {Client} client
+
+     * @param {CommandInteraction} interaction
+
+     * @param {String[]} args
+
+     */
+
+    run: async (client, interaction, args) => {
+
+        interaction.followUp({ content: `${client.ws.ping}ms!` });
+
+    },
+
+};
+
